22c94cc84637e7af94719a5dd568641543eace2d
Joël Spaltenstein <spalte@naturalimage.ch>
2019-11-23 20:09:04 +0100

    pdf wado

--- src/main/java/online/kheops/proxy/wadouri/WadoUriResource.java
+++ src/main/java/online/kheops/proxy/wadouri/WadoUriResource.java
@@ -4,8 +4,12 @@ import online.kheops.proxy.id.SeriesID;
 import online.kheops.proxy.tokens.AccessToken;
 import online.kheops.proxy.tokens.AccessTokenException;
 import online.kheops.proxy.tokens.AuthorizationToken;
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Tag;
+import org.dcm4che3.io.DicomInputStream;
 import org.dcm4che3.mime.MultipartInputStream;
 import org.dcm4che3.mime.MultipartParser;
+import org.dcm4che3.ws.rs.MediaTypes;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.*;
@@ -46,8 +50,14 @@ public class WadoUriResource {
 
     @GET
     @Path("/password/dicomweb/wado")
-    public Response wado(@HeaderParam(AUTHORIZATION) String authorizationHeader) {
-        return webAccess(AuthorizationToken.fromAuthorizationHeader(authorizationHeader));
+    public Response wado(@HeaderParam(AUTHORIZATION) String authorizationHeader, @QueryParam("contentType") String contentTypeParam) {
+        if (contentTypeParam != null && contentTypeParam.equals("application/dicom")) {
+            return webAccess(AuthorizationToken.fromAuthorizationHeader(authorizationHeader));
+        } else if (contentTypeParam != null && contentTypeParam.equals("application/pdf")) {
+            return pdfWebAccess(AuthorizationToken.fromAuthorizationHeader(authorizationHeader));
+        } else {
+            throw new BadRequestException("Bad contentType parameter");
+        }
     }
 
     @GET
@@ -149,6 +159,113 @@ public class WadoUriResource {
 
         return Response.ok(streamingOutput).type(APPLICATION_DICOM).build();
     }
+
+    private Response pdfWebAccess(AuthorizationToken authorizationToken) {
+        final URI authorizationURI = getParameterURI("online.kheops.auth_server.uri");
+        final URI serviceURI = getParameterURI("online.kheops.pacs.uri");
+
+        final MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
+
+        final List<String> studyInstanceUIDs = queryParameters.get("studyUID");
+        if (studyInstanceUIDs == null || studyInstanceUIDs.size() != 1) {
+            LOG.log(WARNING, "Missing studyUID");
+            throw new BadRequestException("Missing studyUID");
+        }
+        final List<String> seriesInstanceUIDs = queryParameters.get("seriesUID");
+        if (seriesInstanceUIDs == null || seriesInstanceUIDs.size() != 1) {
+            LOG.log(WARNING, "Missing seriesUID");
+            throw new BadRequestException("Missing seriesUID");
+        }
+
+        final String studyInstanceUID = studyInstanceUIDs.get(0);
+        final String seriesInstanceUID = seriesInstanceUIDs.get(0);
+
+        final WebTarget instancesTarget = CLIENT.target(serviceURI)
+                .path("/studies/{StudyInstanceUID}/series/{SeriesInstanceUID}/instances")
+                .resolveTemplate("StudyInstanceUID", studyInstanceUID)
+                .resolveTemplate("SeriesInstanceUID", seriesInstanceUID);
+
+        final List<Attributes> instanceList;
+        try {
+            instanceList = instancesTarget.request(MediaTypes.APPLICATION_DICOM_JSON_TYPE)
+                    .header(AUTHORIZATION, authorizationToken.getHeaderValue())
+                    .get(new GenericType<List<Attributes>>() {});
+        } catch (ProcessingException e) {
+            LOG.log(SEVERE, "Unable to get instances", e);
+            throw new ServerErrorException("Unable to get instances", BAD_GATEWAY, e);
+        }
+
+        if (instanceList.size() != 1) {
+            LOG.log(SEVERE, "Not a single instance");
+            throw new NotFoundException("Not a single instance");
+        }
+
+        final String sopInstanceUID = instanceList.get(0).getString(Tag.SOPInstanceUID);
+        if (sopInstanceUID == null) {
+            LOG.log(WARNING, "can't find sopInstanceUID");
+            throw new BadRequestException("can't find sopInstanceUID");
+        }
+
+        final AccessToken accessToken;
+        try {
+            accessToken = AccessToken.createBuilder(authorizationURI)
+                    .withClientId(context.getInitParameter("online.kheops.client.dicomwebproxyclientid"))
+                    .withClientSecret(context.getInitParameter("online.kheops.client.dicomwebproxysecret"))
+                    .withCapability(authorizationToken.getToken())
+                    .withSeriesID(new SeriesID(studyInstanceUID, seriesInstanceUID))
+                    .build();
+        } catch (AccessTokenException e) {
+            LOG.log(WARNING, "Unable to get an access token", e);
+            throw new NotAuthorizedException("Bearer", "Basic");
+        } catch (Exception e) {
+            LOG.log(SEVERE, "unknown error while getting an access token", e);
+            throw new InternalServerErrorException("unknown error while getting an access token");
+        }
+
+        final WebTarget webTarget = CLIENT.target(serviceURI)
+                .path("/studies/{StudyInstanceUID}/series/{SeriesInstanceUID}/instances/{SOPInstanceUID}")
+                .resolveTemplate("StudyInstanceUID", studyInstanceUID)
+                .resolveTemplate("SeriesInstanceUID", seriesInstanceUID)
+                .resolveTemplate("SOPInstanceUID", sopInstanceUID);
+
+        Invocation.Builder invocationBuilder = webTarget.request();
+        invocationBuilder.header(AUTHORIZATION, accessToken.getHeaderValue());
+
+        StreamingOutput streamingOutput = output -> {
+            MultipartParser.Handler handler = (int partNumber, MultipartInputStream in) -> {
+                if (partNumber != 1) {
+                    LOG.log(SEVERE, "Unexpected part number:" + partNumber);
+                    throw new IOException("Unexpected part number:" + partNumber);
+                }
+
+                in.readHeaderParams();
+
+                final DicomInputStream dicomInputStream = new DicomInputStream(in);
+
+                final Attributes attributes = dicomInputStream.readDataset(-1, -1);
+                final byte[] documentBytes = attributes.getBytes(Tag.EncapsulatedDocument);
+
+                output.write(documentBytes);
+            };
+
+            try (final Response wadoRSResponse = webTarget.request().header(AUTHORIZATION, accessToken.getHeaderValue()).get()) {
+                final String boundary = MediaType.valueOf(wadoRSResponse.getHeaderString(CONTENT_TYPE)).getParameters().get(BOUNDARY_PARAMETER);
+                try (final InputStream inputStream = new BufferedInputStream(wadoRSResponse.readEntity(InputStream.class))) {
+                    new MultipartParser(boundary).parse(inputStream, handler);
+                }
+            } catch (ResponseProcessingException e) {
+                LOG.log(SEVERE, "ResponseProcessingException status:" + e.getResponse().getStatus(), e);
+                throw new IOException(e);
+            } catch (ProcessingException e) {
+                LOG.log(SEVERE, "ProcessingException:", e);
+                throw new IOException(e);
+            }
+            output.flush();
+        };
+
+        return Response.ok(streamingOutput).type("application/pdf").build();
+    }
+
 
     private URI getParameterURI(String parameter) {
         try {
